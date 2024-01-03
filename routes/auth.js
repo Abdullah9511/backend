bdd558a469e0d6e28edf5c9b69d5c4f8e187fa5f
Abdullah9511 <abdullah.bin.idrees@gmail.com>
2024-01-04 04:43:17 +0500

    Databae connectivity

--- routes/auth.js
+++ routes/auth.js
@@ -1,15 +1,24 @@
 const express = require("express");
 const User = require('../models/User')
-
+const { body, validationResult } = require('express-validator');
 const router = express.Router()
 
 // Create a User using POST-"/api/auth" Does not require auth 
 
-router.get('/', (req, res) => {
-    console.log(req.body)
-    const user = User(req.body)
-    user.save()
-    res.send('hello')
+router.post('/', [
+    body('name').isLength({ min:  3}),
+    body('email').isEmail(),
+    body('password').isLength({ min: 5 }),
+],(req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    User.create({
+        name: req.body.name,
+        password: req.body.password,
+        email: req.body.email,
+      }).then(user => res.json(user));
 
 })
 
